f9d0d1519abee985c88e826771ce1bfcde1661df
elisagigliotti <elisagigliotti2002@gmail.com>
2024-08-10 12:02:31 +0200

    backend: Initial commit

---
+++ src/main/java/it/unical/inf/ea/sefora_backend/SeforaBackendApplication.java
@@ -0,0 +1,13 @@
+package it.unical.inf.ea.sefora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeforaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeforaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unical/inf/ea/sefora_backend/SeforaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.inf.ea.sefora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeforaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
